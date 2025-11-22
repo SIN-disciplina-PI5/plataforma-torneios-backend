12558e567788ffb77d6ecce7beb5dfb11d8bffee
Pedro Delmiro <163359112+PedroDelmiro13@users.noreply.github.com>
2025-11-22 17:13:51 +0000

    feat: adding rate limiter middleware

--- plataforma-torneio/api/routes/userRoute.js
+++ plataforma-torneio/api/routes/userRoute.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { authenticateToken } from "../middlewares/authenticateToken.js";
+import { rateLimiter } from "../middlewares/rateLimiter.js";
 import { login, logout } from "../controllers/authController.js";
 import { criarUsuario, editarPerfil, deletarPerfil } from "../controllers/userController.js";
 
 const router = Router();
 
-router.post("/login", login);
+router.post("/login", rateLimiter, login);
 router.post("/logout", authenticateToken, logout);
-router.post("/signup", criarUsuario);
-router.patch("/edit/:id_usuario", authenticateToken, editarPerfil);
-router.delete("/delete/:id_usuario", authenticateToken, deletarPerfil);
+router.post("/signup", rateLimiter, criarUsuario);
+router.patch("/edit/:id_usuario", authenticateToken, rateLimiter, editarPerfil);
+router.delete("/delete/:id_usuario", authenticateToken, rateLimiter, deletarPerfil);
 
 export default router;
