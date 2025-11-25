7762d0f4e52774bf8db71140a952300c51347470
odaniellevi <daniellevi.unicap@gmail.com>
2025-11-25 20:27:46 -0300

    api/routes/index.js concluido

--- plataforma-torneio/api/routes/index.js
+++ plataforma-torneio/api/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import userRoute from "./userRoute.js";
 import torneioRoute from "./torneioRoute.js";
 import adminRoute from "./adminRoute.js";
+import equipeRoute from "./equipeRoute.js";
 
 const router = Router();
 
 router.use("/users", userRoute);
 router.use("/torneio", torneioRoute);
 router.use("/admin", adminRoute);
+router.use("/equipe", equipeRoute);
 
 export default router;
