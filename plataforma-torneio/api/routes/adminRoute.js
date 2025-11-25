0894cb31845ce92eb00bb3c8abe59ac2f6a6d8d1
odaniellevi <daniellevi.unicap@gmail.com>
2025-11-25 16:01:50 -0300

    adminRoute.js atualizado

--- plataforma-torneio/api/routes/adminRoute.js
+++ plataforma-torneio/api/routes/adminRoute.js
@@ -3,10 +3,7 @@ import { registerAdmin, loginAdmin } from "../controllers/adminController.js";
 
 const router = Router();
 
-// Endpoint: POST /api/admin/register
 router.post("/register", registerAdmin);
-
-// Endpoint: POST /api/admin/login
 router.post("/login", loginAdmin);
 
 export default router;
