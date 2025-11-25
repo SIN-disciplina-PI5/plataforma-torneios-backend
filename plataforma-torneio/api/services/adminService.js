dd2e6dd79fbd9ab5bc7b847675d988d013a99722
odaniellevi <daniellevi.unicap@gmail.com>
2025-11-25 17:59:11 -0300

    adminService.js atualizado

--- plataforma-torneio/api/services/adminService.js
+++ plataforma-torneio/api/services/adminService.js
@@ -1,4 +1,5 @@
 import models from "../models/index.js";
+import bcrypt from "bcrypt";
 const { Usuario } = models;
 
 export const createAdminService = async (dados) => {
@@ -27,4 +28,39 @@ export const createAdminService = async (dados) => {
     email: novoAdmin.email,
     role: novoAdmin.role,
   };
+};
+
+export const updateAdminService = async (id, dados) => {
+  const admin = await Usuario.findByPk(id);
+  if (!admin) {
+    throw new Error("Administrador não encontrado.");
+  }
+
+  // Se o usuário mandou uma nova senha, precisamos criptografar manualmente
+  // pois o hook 'beforeCreate' só roda na criação.
+  if (dados.senha) {
+    dados.senha = await bcrypt.hash(dados.senha, 12);
+  }
+
+  // Atualiza os dados
+  await admin.update(dados);
+
+  // Retorna os dados atualizados (sem a senha)
+  return {
+    id_usuario: admin.id_usuario,
+    nome: admin.nome,
+    email: admin.email,
+    role: admin.role,
+  };
+};
+
+// Deletar Admin
+export const deleteAdminService = async (id) => {
+  const admin = await Usuario.findByPk(id);
+  if (!admin) {
+    throw new Error("Administrador não encontrado.");
+  }
+
+  await admin.destroy();
+  return { message: "Administrador deletado com sucesso." };
 };
