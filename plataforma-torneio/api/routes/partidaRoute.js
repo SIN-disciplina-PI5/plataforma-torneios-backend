82db6b4696bcc3e06ab71dc4ce1e04568cc55fb3
Mel Lopes <163359225+mellopes06br@users.noreply.github.com>
2025-11-30 19:16:42 +0000

    ajustando middleware

--- plataforma-torneio/api/routes/partidaRoute.js
+++ plataforma-torneio/api/routes/partidaRoute.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { authenticateToken } from "../middlewares/authenticateToken.js";
 import { rateLimiter } from "../middlewares/rateLimiter.js";
-import { isAdmin } from "../middlewares/authorize.js";
+import { checkAdmin } from "../middlewares/checkAdmin.js";
 import {
   criarPartida,
   buscarPartida,
@@ -17,15 +17,15 @@ import {
 
 const router = Router();
 
-router.post("/", authenticateToken, isAdmin, rateLimiter, criarPartida);
+router.post("/", authenticateToken, checkAdmin, rateLimiter, criarPartida);
 router.get("/", authenticateToken, listarPartidas);
 router.get("/:id_partida", authenticateToken, buscarPartida);
-router.patch("/edit/:id_partida", authenticateToken, isAdmin, rateLimiter, editarPartida);
-router.delete("/delete/:id_partida", authenticateToken, isAdmin, rateLimiter, deletarPartida);
-router.patch("/agendar/:id_partida", authenticateToken, isAdmin, rateLimiter, agendar);
-router.patch("/iniciar/:id_partida", authenticateToken, isAdmin, rateLimiter, iniciar);
-router.patch("/resultado/:id_partida", authenticateToken, isAdmin, rateLimiter, registrarResultado);
-router.patch("/vencedor/:id_partida", authenticateToken, isAdmin, rateLimiter, definirVencedor);
-router.patch("/finalizar/:id_partida", authenticateToken, isAdmin, rateLimiter, finalizar);
+router.patch("/edit/:id_partida", authenticateToken, checkAdmin, rateLimiter, editarPartida);
+router.delete("/delete/:id_partida", authenticateToken, checkAdmin, rateLimiter, deletarPartida);
+router.patch("/agendar/:id_partida", authenticateToken, checkAdmin, rateLimiter, agendar);
+router.patch("/iniciar/:id_partida", authenticateToken, checkAdmin, rateLimiter, iniciar);
+router.patch("/resultado/:id_partida", authenticateToken, checkAdmin, rateLimiter, registrarResultado);
+router.patch("/vencedor/:id_partida", authenticateToken, checkAdmin, rateLimiter, definirVencedor);
+router.patch("/finalizar/:id_partida", authenticateToken, checkAdmin, rateLimiter, finalizar);
 
 export default router;
