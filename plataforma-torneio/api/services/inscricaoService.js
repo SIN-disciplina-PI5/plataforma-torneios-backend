4f1843a631a68f53e518ffd07bdbdcd683cc4b58
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2025-12-01 13:28:42 -0300

    retirando comentários de inscricao service

--- plataforma-torneio/api/services/inscricaoService.js
+++ plataforma-torneio/api/services/inscricaoService.js
@@ -7,12 +7,12 @@ export const createInscricaoService = async ({ id_equipe, id_torneio }) => {
     throw new Error("Dados faltando para realizar a inscrição");
   }
 
-  //const equipe = await Equipe.findByPk(id_equipe);
+  const equipe = await Equipe.findByPk(id_equipe);
   const torneio = await Torneio.findByPk(id_torneio);
 
-  /*if (!equipe || !torneio) {
-        throw new Error("Equipe ou Torneio não encontrados");
-    }*/
+  if (!equipe || !torneio) {
+    throw new Error("Equipe ou Torneio não encontrados");
+  }
 
   const inscricaoExistente = await Inscricao.findOne({
     where: { id_equipe, id_torneio },
