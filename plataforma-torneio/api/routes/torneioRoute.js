53d5ccd2a88a1372b256f6336757dc06375155ad
Pedro Delmiro <163359112+PedroDelmiro13@users.noreply.github.com>
2025-12-03 16:28:01 +0000

    fix: fix controller router adding middlewares

--- plataforma-torneio/api/routes/torneioRoute.js
+++ plataforma-torneio/api/routes/torneioRoute.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { criarTorneio, listarTorneios, buscarTorneio, atualizarTorneio, deletarTorneio,} from "../controllers/torneioController.js";
 import { authenticateToken } from "../middlewares/authenticateToken.js";
+import checkAdmin from "../middlewares/checkAdmin.js";
 
 const router = Router();
 
-router.post("/", authenticateToken, criarTorneio);
-router.get("/", listarTorneios);
-router.get("/:id_torneio", buscarTorneio);
-router.patch("/:id_torneio", authenticateToken, atualizarTorneio);
-router.delete("/:id_torneio", authenticateToken, deletarTorneio);
+router.post("/", authenticateToken, checkAdmin, criarTorneio);
+router.get("/", authenticateToken, checkAdmin, listarTorneios);
+router.get("/:id_torneio", authenticateToken, checkAdmin, buscarTorneio);
+router.patch("/:id_torneio", authenticateToken, checkAdmin, atualizarTorneio);
+router.delete("/:id_torneio", authenticateToken,  checkAdmin, deletarTorneio);
 
 export default router;
