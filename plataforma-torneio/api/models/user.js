8bca4a59a64abda1cda382e7dda219a05dfb575a
Kaiki Barros <104598739+kaikibarros@users.noreply.github.com>
2025-12-05 19:22:46 -0300

    Refactor User model to Usuario with password hashing

--- plataforma-torneio/api/models/user.js
+++ plataforma-torneio/api/models/user.js
@@ -1,26 +1,50 @@
 import { DataTypes } from "sequelize";
-import { sequelize } from "../config/database.js";
+import bcrypt from "bcrypt";
 
-export const User = sequelize.define("User", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
+export default (sequelize) => {
+  const Usuario = sequelize.define("Usuario", {
+    id_usuario: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { notEmpty: true },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { isEmail: true, notEmpty: true },
+    },
+    senha: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { notEmpty: true },
+    },
+    patente: {
+      type: DataTypes.STRING,
+      allowNull: true, //por enquanto
+    },
+    role: {
+      type: DataTypes.ENUM("ADMIN", "USER"),
+      allowNull: false,
+      validate: { notEmpty: true },
+    },
+  });
+Usuario.beforeCreate(async (usuario) => {
+  usuario.senha = await bcrypt.hash(usuario.senha, 12);
+  if (!usuario.role) {
+    usuario.role = "USER";
+  }
 });
+
+Usuario.beforeUpdate(async (usuario) => {
+  if (usuario.changed('senha')) {
+    usuario.senha = await bcrypt.hash(usuario.senha, 12);
+  }
+});
+  return Usuario;
+};
