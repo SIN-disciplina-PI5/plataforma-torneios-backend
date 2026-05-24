7673e98dd8d033557ac8f9550615f5e9bc3e0663
PedroDelmiro13 <pdelmiro1309@gmail.com>
2026-05-23 22:53:00 -0300

    .

--- plataforma-torneio/api/routes/equipeRoute.js
+++ plataforma-torneio/api/routes/equipeRoute.js
@@ -17,11 +17,11 @@ const router = Router();
 
 router.post("/admin/:id_equipe/membros", authenticateToken, checkAdmin, adminAddMembro);
 router.delete("/admin/:id_equipe/membros/:id_usuario", authenticateToken, checkAdmin, adminRemoveMembro);
+router.post("/entrar/:id_torneio", authenticateToken, entrarNaEquipe);
+router.post("/sair/:id_torneio", authenticateToken, sairDaEquipe);
 router.get("/", authenticateToken, getAllEquipes);
 router.get("/:id", authenticateToken, getEquipeById);
 router.post("/:id_torneio", authenticateToken, createEquipe);
-router.post("/entrar/:id_torneio", authenticateToken, entrarNaEquipe);
-router.post("/sair/:id_torneio", authenticateToken, sairDaEquipe);
 router.put("/:id", authenticateToken, checkAdmin, updateEquipe);
 router.delete("/:id", authenticateToken, checkAdmin, deleteEquipe);
 
