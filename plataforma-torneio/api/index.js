69d0f60189611f581f5dcf6ab6cd8797d557faf5
PedroDelmiro13 <pdelmiro1309@gmail.com>
2026-06-03 13:15:12 -0300

    permitindo por enquanto

--- plataforma-torneio/api/index.js
+++ plataforma-torneio/api/index.js
@@ -10,9 +10,10 @@ if (process.env.NODE_ENV !== "test") {
   app.set("trust proxy", true);
 }
 
-const allowedOrigins = process.env.CORS_ORIGINS?.split(",") || [];
+//const allowedOrigins = process.env.CORS_ORIGINS?.split(",") || [];
 
-app.use(cors({ origin: allowedOrigins,}),);
+//app.use(cors({ origin: allowedOrigins,}),);
+app.use(cors());
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
