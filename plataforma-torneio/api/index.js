06dd72dd1b905056d6b2f7a21b97628e7d2d5dbf
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2026-06-04 20:04:33 -0300

    tentando cord com url no index

--- plataforma-torneio/api/index.js
+++ plataforma-torneio/api/index.js
@@ -8,10 +8,22 @@ const app = express();
 
 app.set("trust proxy", true);
 
-//const allowedOrigins = process.env.CORS_ORIGINS?.split(",") || [];
+const allowedOrigins = [
+  "https://arena-lagoa-beach-git-developer-kaiki-barros-projects.vercel.app",
+  "https://gabssiin-lagoa-ai-2.hf.space/api/chat",
+];
 
-//app.use(cors({ origin: allowedOrigins,}),);
-app.use(cors());
+app.use(
+  cors({
+    origin(origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Origem nao permitida pelo CORS"));
+    },
+  }),
+);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
